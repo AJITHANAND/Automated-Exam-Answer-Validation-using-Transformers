e4a83e2af8974e03a41b14bac7936fd7c0a65ce4
Ajith Anand <ajithanand303@gmail.com>
2023-04-20 15:17:16 +0530

    questions database design

--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Questions(models.Model):
+    question = models.CharField(max_length=200)
+    answer = models.CharField(max_length=2000)
+    std = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question
