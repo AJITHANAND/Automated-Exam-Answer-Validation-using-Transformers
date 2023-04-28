fcdefd3b76faa550603bada43272f3577bb44d18
Ajith Anand <ajithanand303@gmail.com>
2023-04-28 14:16:34 +0530

    Teacher dashboard

--- teacher/models.py
+++ teacher/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 from django.db.models import Max
 
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    admin_code = models.CharField(max_length=200, default="EXAM-Controller")
+
+    def __str__(self):
+        return self.name
+
+
 class Paper(models.Model):
-    paper_code = models.CharField(max_length=200 )
+    paper_code = models.CharField(max_length=200)
     std = models.CharField(max_length=200)
     is_active = models.BooleanField(default=True)
+
+
 # Create your models here.
 class Questions(models.Model):
     question = models.CharField(max_length=200)
@@ -16,7 +30,8 @@ class Questions(models.Model):
     def save(self, *args, **kwargs):
         if not self.pk:
             # If the instance is new, get the maximum question_num for the paper_code
-            max_question_num = Questions.objects.filter(paper_code=self.paper_code).aggregate(Max('question_num'))['question_num__max']
+            max_question_num = Questions.objects.filter(paper_code=self.paper_code).aggregate(Max('question_num'))[
+                'question_num__max']
             if max_question_num is None:
                 # If this is the first question for the paper_code, start from 1
                 self.question_num = 1
--- teacher/urls.py
+++ teacher/urls.py
@@ -1,21 +1,13 @@
-"""Exam URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-
+    path('', views.home, name="teacher_home"),
+    path('login', views.login, name="teacher_login"),
+    path('exam-questions', views.exam_question_paper, name='teacher_exam'),
+    path('term-update', views.terms_modify, name='term_modify'),
+    path('term-delete',views.delete_term,name="term_delete"),
+    path('question-add',views.add_question,name="add_question"),
+    path('result',views.show_results,name="show_results"),
 ]
